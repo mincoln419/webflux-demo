8a457dc638201ef75fc8cd6accbe358fa95fbc6c
mermer vivo <mincoln419@naver.com>
2024-05-30 13:02:45 +0900

    기본 틀 개발

---
+++ src/main/java/com/ideatec/springwebfluxdemo/controller/ServerController.java
@@ -0,0 +1,24 @@
+package com.ideatec.springwebfluxdemo.controller;
+
+import com.ideatec.springwebfluxdemo.entity.Dish;
+import com.ideatec.springwebfluxdemo.service.KitchenService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ServerController {
+
+    private final KitchenService kitchenService;
+
+    public ServerController(KitchenService kitchenservice){
+        this.kitchenService = kitchenservice;
+    }
+
+    @GetMapping(value = "/server", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Dish> servDishes(){
+        return this.kitchenService.getDishes();
+
+    }
+}
